5a8a9a139e62586b8fd7975e45aee27bcc41fc90
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2022-10-29 22:37:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const DELAY = 1000;
+const body = document.body;
+let interval = null;
+
+refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+}
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function onBtnStartClick(){
+    refs.btnStart.classList.add('disabled', true);
+    refs.btnStop.classList.remove('disabled');
+    //console.log('Клік по кнопці Start');
+
+    interval = setInterval(()=>{
+       //console.log('timeOut');
+       body.style.backgroundColor = getRandomHexColor();
+    }, DELAY);
+}
+
+function onBtnStopClick(){
+    refs.btnStart.classList.remove('disabled');
+    refs.btnStop.classList.add('disabled', true);
+    //console.log('Клік по кнопці Stop');
+    clearInterval(interval);
+}
+
+
+
 
